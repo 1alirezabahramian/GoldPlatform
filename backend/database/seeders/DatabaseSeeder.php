5f5ccf748d3edd993e73a0c3b1584eb5d26c6f3b
1alirezabahramian <131552738+1alirezabahramian@users.noreply.github.com>
2026-08-04 21:10:16 +0330

    feat(admin): register management permission seeder

--- backend/database/seeders/DatabaseSeeder.php
+++ backend/database/seeders/DatabaseSeeder.php
@@ -15,6 +15,8 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
+        $this->call(AdminOperatorPermissionSeeder::class);
+
         // User::factory(10)->create();
 
         User::factory()->create([
