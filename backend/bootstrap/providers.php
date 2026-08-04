ddadc59e8e99570f3e250aa04e414af552fad77e
1alirezabahramian <131552738+1alirezabahramian@users.noreply.github.com>
2026-08-04 22:12:57 +0330

    feat(trading): register trading service provider

--- backend/bootstrap/providers.php
+++ backend/bootstrap/providers.php
@@ -2,8 +2,10 @@
 
 use App\Providers\AppServiceProvider;
 use App\Providers\FinancialServiceProvider;
+use App\Providers\TradingServiceProvider;
 
 return [
     AppServiceProvider::class,
     FinancialServiceProvider::class,
+    TradingServiceProvider::class,
 ];
